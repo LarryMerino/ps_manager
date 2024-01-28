b7cf5e338f3993261c0b27a5778ad6f9a2e386d8
Larry Merino <merino17@icloud.com>
2024-01-28 17:51:15 +0100

    refactor: modify the configuration struct
    Implement dependency inversion by using traits in the configuration struct
    Move the configuration files to be separate from the source files

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use simple_logger::SimpleLogger;
-use crate::settings::Settings;
+use indicatif::MultiProgress;
+use indicatif_log_bridge::LogWrapper;
+use log::{debug, info};
 
-mod settings;
+use crate::settings::{Settings, SettingsProvider};
+pub mod settings;
+
+// Check this for some inspiration
+// https://github.com/JaynewDee/portform/blob/866727ad3cd6ff499844d54b673472f646c95a40/src/generate.rs#L4
 
 fn main() {
-    SimpleLogger::new()
-        .with_threads(true)
-        .init()
-        .unwrap();
+    let settings = Settings::new().unwrap();
+    let logger = env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or(settings.get_conf_provider().get_debug_lvl()),
+    )
+    .build();
 
-    let settings = Settings::new();
+    let multi = MultiProgress::new();
+    LogWrapper::new(multi.clone(), logger).try_init().unwrap();
 
-    // Print out our settings
-    println!("{:?}", settings);
+    info!("Logger initialized");
+    debug!("{:#?}", settings);
 
+    info!("Finish Job");
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,25 +1,55 @@
+use std::env;
+
 use config::{Config, ConfigError, File};
 use serde_derive::Deserialize;
-use std::env;
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+struct Host {
+    url: String,
+    key: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+pub struct DebugConf {
+    debug_lvl: String,
+}
+
+pub trait DebugConfProvider: std::fmt::Debug {
+    fn get_debug_lvl(&self) -> String;
+}
+
+impl DebugConfProvider for DebugConf {
+    fn get_debug_lvl(&self) -> String {
+        self.debug_lvl.clone()
+    }
+}
 
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
 pub struct Settings {
-    url: String,
-    token: String,
+    host: Host,
+    debug: DebugConf,
+}
+
+pub trait SettingsProvider: std::fmt::Debug{
+    fn get_conf_provider(&self) -> &dyn DebugConfProvider;
+}
+
+impl SettingsProvider for Settings {
+    fn get_conf_provider(&self) -> &dyn DebugConfProvider {
+        &self.debug
+    }
 }
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
-        // Add as many configurations as you need within the 'config' folder
-        // It's recommended to use environment variables or other methods to add sensitive data
-        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
-        log::info!("run_mode: {}", run_mode);
+        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "default".into());
 
-        Config::builder()
-            .add_source(File::with_name(&format!("src/config/{}", run_mode)))
-            .build()
-            .unwrap()
-            .try_deserialize()
+        let conf = Config::builder()
+            .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
+            .build()?;
+        conf.try_deserialize()
     }
 }
